3b81c22a8377e8e93a11ccc8e3dcf865c10135ad
Enoch <mmejuenoch@gmail.com>
2024-02-17 16:39:40 +0100

    fixed layout issue of the 'more_info_sheet'

--- src/app/page_sections/more_info_sheet.tsx
+++ src/app/page_sections/more_info_sheet.tsx
@@ -18,20 +18,24 @@ export default function InfoSheet() {
           Learn more
         </button>
       </SheetTrigger>
-      <SheetContent className="w-full sm:max-w-2xl">
-        {/* <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader> */}
-        <Tabs defaultValue="attention" className="w-full mt-6 border">
-          <TabsList className="border border-pink-300">
-            <TabsTrigger value="attention">Attention</TabsTrigger>
-            <TabsTrigger value="mission">Our mission</TabsTrigger>
-            <TabsTrigger value="vision">Our vision</TabsTrigger>
-            <TabsTrigger value="crew">The crew</TabsTrigger>
+      <SheetContent className="w-full px-2 sm:max-w-2xl">
+        <Tabs
+          defaultValue="attention"
+          className="w-full sm:flex sm:flex-col sm:items-center mt-6"
+        >
+          <TabsList className="max-sm:w-full max-sm:justify-between">
+            <TabsTrigger value="attention" className="px-2 max-[380px]:text-xs">
+              Attention
+            </TabsTrigger>
+            <TabsTrigger value="mission" className="px-2 max-[380px]:text-xs">
+              Our mission
+            </TabsTrigger>
+            <TabsTrigger value="vision" className="px-2 max-[380px]:text-xs">
+              Our vision
+            </TabsTrigger>
+            <TabsTrigger value="crew" className="px-2 max-[380px]:text-xs">
+              The crew
+            </TabsTrigger>
           </TabsList>
           {/*attention*/}
           <TabsContent value="attention" className="w-full">
